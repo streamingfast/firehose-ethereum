34d905421e96a6e8c6b1ee93e09e76a7585aaea8
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-02-24 11:54:59 -0500

    account indexing tool to also produce irreversible blocks and start from where it left off

--- tools/account_index.go
+++ tools/account_index.go
@@ -26,6 +26,7 @@ import (
 	pbfirehose "github.com/streamingfast/pbgo/sf/firehose/v1"
 	pbcodec "github.com/streamingfast/sf-ethereum/pb/sf/ethereum/codec/v1"
 	"github.com/streamingfast/sf-ethereum/transform"
+	"go.uber.org/zap"
 )
 
 var generateAccIdxCmd = &cobra.Command{
@@ -37,28 +38,80 @@ var generateAccIdxCmd = &cobra.Command{
 }
 
 func init() {
-	generateAccIdxCmd.Flags().IntSlice("bundle-sizes", []int{10000}, "list of sizes for irreversible block indices")
+	generateAccIdxCmd.Flags().Uint64("account-indexes-size", 10000, "size of account index bundles that will be created")
+	generateAccIdxCmd.Flags().IntSlice("lookup-account-indexes-sizes", []int{1000000, 100000, 10000, 1000}, "account index bundle sizes that we will look for on start to find first unindexed block (should include account-indexes-size)")
+	generateAccIdxCmd.Flags().IntSlice("irreversible-indexes-sizes", []int{10000, 1000}, "size of irreversible indexes that will be used")
+	generateAccIdxCmd.Flags().Bool("create-irreversible-indexes", false, "if true, irreversible indexes will also be created")
 	Cmd.AddCommand(generateAccIdxCmd)
+}
+
+func lowBoundary(i uint64, mod uint64) uint64 {
+	return i - (i % mod)
+}
+
+func toIndexFilename(bundleSize, baseBlockNum uint64, shortname string) string {
+	return fmt.Sprintf("%010d.%d.%s.idx", baseBlockNum, bundleSize, shortname)
+}
+
+func skipToNextUnindexed(ctx context.Context, blockNum uint64, possibleIndexSizes []uint64, shortName string, store dstore.Store) (next uint64) {
+	next = blockNum
+
+	for {
+		var found bool
+		for _, size := range possibleIndexSizes {
+			base := lowBoundary(next, size)
+			filename := toIndexFilename(size, base, shortName)
+
+			exists, err := store.FileExists(ctx, filename)
+			if err != nil && err != dstore.ErrNotFound {
+				zlog.Warn("store lookup returned an error", zap.Error(err))
+			}
+			if exists {
+				found = true
+				next += size
+				break
+			}
+		}
+		if !found {
+			return
+		}
+	}
 }
 
 func generateAccIdxE(cmd *cobra.Command, args []string) error {
 
-	sizes, err := cmd.Flags().GetIntSlice("bundle-sizes")
+	iis, err := cmd.Flags().GetIntSlice("irreversible-indexes-sizes")
 	if err != nil {
 		return err
 	}
-	var bundleSizes []uint64
-	for _, size := range sizes {
+	var irrIdxSizes []uint64
+	for _, size := range iis {
 		if size < 0 {
 			return fmt.Errorf("invalid negative size for bundle-sizes: %d", size)
 		}
-		bundleSizes = append(bundleSizes, uint64(size))
+		irrIdxSizes = append(irrIdxSizes, uint64(size))
+	}
+
+	acctIdxSize, err := cmd.Flags().GetUint64("account-indexes-size")
+	if err != nil {
+		return err
+	}
+	lais, err := cmd.Flags().GetIntSlice("lookup-account-indexes-sizes")
+	if err != nil {
+		return err
+	}
+	var lookupAccountIdxSizes []uint64
+	for _, size := range lais {
+		if size < 0 {
+			return fmt.Errorf("invalid negative size for bundle-sizes: %d", size)
+		}
+		lookupAccountIdxSizes = append(lookupAccountIdxSizes, uint64(size))
 	}
 
 	accountIndexStoreURL := args[0]
 	irrIndexStoreURL := args[1]
 	blocksStoreURL := args[2]
-	startBlockNum, err := strconv.ParseInt(args[3], 10, 64)
+	startBlockNum, err := strconv.ParseUint(args[3], 10, 64)
 	if err != nil {
 		return fmt.Errorf("unable to parse block number %q: %w", args[0], err)
 	}
@@ -91,8 +144,8 @@ func generateAccIdxE(cmd *cobra.Command, args []string) error {
 		zlog,
 		[]dstore.Store{blocksStore},
 		irrIndexStore,
-		false,
-		bundleSizes,
+		true,
+		irrIdxSizes,
 		nil,
 		nil,
 		nil,
@@ -100,15 +153,18 @@ func generateAccIdxE(cmd *cobra.Command, args []string) error {
 	)
 
 	ctx := context.Background()
+
+	startBlockNum = skipToNextUnindexed(ctx, uint64(startBlockNum), lookupAccountIdxSizes, transform.LogAddressIdxShortname, accountIndexStore)
+
 	cli := firehoseServer.BlocksFromLocal(ctx, &pbfirehose.Request{
-		StartBlockNum: startBlockNum,
+		StartBlockNum: int64(startBlockNum),
 		StopBlockNum:  stopBlockNum,
 		ForkSteps:     []pbfirehose.ForkStep{pbfirehose.ForkStep_STEP_IRREVERSIBLE},
 	})
 
 	cmd.SilenceUsage = true
 
-	t := transform.NewLogAddressIndexer(accountIndexStore, bundleSizes[0]) // FIXME add support for multiple bundlesizes?
+	t := transform.NewLogAddressIndexer(accountIndexStore, acctIdxSize)
 
 	for {
 		resp, err := cli.Recv()
