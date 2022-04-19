2ca5892f99774b36db8809435dc83d6e6b521031
Matthieu Vachon <matt@streamingfast.io>
2022-04-19 11:25:49 -0400

    Removed ct package in pb/sf...

--- types/pb/sf/ethereum/type/v1/testing/logging.go
+++
@@ -1,5 +0,0 @@
-package ct
-
-import "github.com/streamingfast/logging"
-
-var zlog, _ = logging.PackageLogger("codec.testing", "github.com/streamingfast/sf-ethereum/types/pb/sf/ethereum/type/v1/testing")
