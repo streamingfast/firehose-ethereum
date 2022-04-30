2708cc55a7cea09f75c805f9f0551723ffa30511
Matthieu Vachon <matt@streamingfast.io>
2022-04-29 23:08:32 -0400

    Fixed standard devel config and added sync-goerli config

---
+++ devel/sync-goerli/start.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+clean=
+sfeth="$ROOT/../sfeth"
+
+main() {
+  pushd "$ROOT" &> /dev/null
+
+  while getopts "hc" opt; do
+    case $opt in
+      h) usage && exit 0;;
+      c) clean=true;;
+      \?) usage_error "Invalid option: -$OPTARG";;
+    esac
+  done
+  shift $((OPTIND-1))
+  [[ $1 = "--" ]] && shift
+
+  set -e
+
+  if [[ $clean == "true" ]]; then
+    rm -rf sf-data &> /dev/null || true
+  fi
+
+  exec $sfeth -c $(basename $ROOT).yaml start "$@"
+}
+
+usage_error() {
+  message="$1"
+  exit_code="$2"
+
+  echo "ERROR: $message"
+  echo ""
+  usage
+  exit ${exit_code:-1}
+}
+
+usage() {
+  echo "usage: start.sh [-c]"
+  echo ""
+  echo "Start $(basename $ROOT) environment."
+  echo ""
+  echo "Options"
+  echo "    -c             Clean actual data directory first"
+  echo ""
+  echo "Examples"
+  echo "   Stream blocks    grpcurl -insecure -import-path ../proto -import-path ../proto-ethereum -proto dfuse/ethereum/codec/v1/codec.proto -proto dfuse/bstream/v1/bstream.proto -d '{\"start_block_num\": -1}' localhost:13042 dfuse.bstream.v1.BlockStreamV2.Blocks"
+}
+
+main "$@"
