f66bb144f82054a4b8efbea0ee735d0d6982479d
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-05-20 13:40:18 -0400

    remove unused flag

--- cmd/sfeth/cli/mindreader-node.go
+++ cmd/sfeth/cli/mindreader-node.go
@@ -48,7 +48,6 @@ func registerMindreaderNodeFlags(cmd *cobra.Command) error {
 
 	cmd.Flags().String("mindreader-node-grpc-listen-addr", MindreaderGRPCAddr, "Address to listen for incoming gRPC requests")
 	cmd.Flags().Bool("mindreader-node-merge-and-store-directly", false, "[BATCH] When enabled, do not write oneblock files, sidestep the merger and write the merged 100-blocks logs directly to --common-blocks-store-url")
-	cmd.Flags().Bool("mindreader-node-discard-after-stop-num", false, "Ignore remaining blocks being processed after stop num (only useful if we discard the mindreader data after reprocessing a chunk of blocks)")
 	cmd.Flags().String("mindreader-node-working-dir", "{sf-data-dir}/mindreader/work", "Path where mindreader will stores its files")
 	cmd.Flags().Uint("mindreader-node-start-block-num", 0, "Blocks that were produced with smaller block number then the given block num are skipped")
 	cmd.Flags().Uint("mindreader-node-stop-block-num", 0, "Shutdown mindreader when we the following 'stop-block-num' has been reached, inclusively.")
