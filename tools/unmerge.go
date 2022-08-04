548eb6c261830ed9fb95af1008cb195d27a1dd69
colindickson <colin.dickson@gmail.com>
2022-08-04 15:34:06 -0400

    unmerge: better error handling

--- tools/unmerge.go
+++ tools/unmerge.go
@@ -36,12 +36,12 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 
 	srcStore, err := dstore.NewDBinStore(args[0])
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to create source store: %w", err)
 	}
 
 	destStore, err := dstore.NewDBinStore(args[1])
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to create destination store: %w", err)
 	}
 
 	start := mustParseUint64(args[2])
@@ -60,13 +60,13 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 
 		rc, err := srcStore.OpenObject(ctx, filename)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to open %s: %w", filename, err)
 		}
 		defer rc.Close()
 
 		reader, err := bstream.GetBlockReaderFactory.New(rc)
 		if err != nil {
-			return err
+			return fmt.Errorf("get block reader: %w", err)
 		}
 
 		for {
@@ -86,9 +86,16 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 			pr, pw := io.Pipe()
 
 			go func(block *bstream.Block) {
-				bw, _ := bstream.GetBlockWriterFactory.New(pw)
-				err := bw.Write(block)
+				bw, err := bstream.GetBlockWriterFactory.New(pw)
 				if err != nil {
+					zlog.Error("error creating block writer", zap.Error(err))
+					pw.CloseWithError(err)
+					return
+				}
+
+				err = bw.Write(block)
+				if err != nil {
+					zlog.Error("error writing block", zap.Error(err))
 					pw.CloseWithError(err)
 					return
 				}
@@ -98,7 +105,7 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 			oneblockFilename := bstream.BlockFileNameWithSuffix(block, "extracted")
 			err = destStore.WriteObject(ctx, oneblockFilename, pr)
 			if err != nil {
-				return err
+				return fmt.Errorf("error writing block %d to %s: %w", block.Number, oneblockFilename, err)
 			}
 			zlog.Info(fmt.Sprintf("wrote block %d to %s", block.Number, oneblockFilename))
 		}
