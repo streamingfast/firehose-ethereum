6a489e5e5b56e20466e8ed2139fe7251c579850d
colindickson <colin.dickson@gmail.com>
2022-08-04 22:02:25 -0400

    unmerge: clean up, comments, usage string

--- tools/unmerge.go
+++ tools/unmerge.go
@@ -13,7 +13,7 @@ import (
 )
 
 var unmergeBlocksCmd = &cobra.Command{
-	Use:   "unmerge <src-store> <dest-store> <start> <stop>",
+	Use:   "unmerge <src-merged-blocks-store> <dest-one-blocks-store> <start-block> <stop-block>",
 	Short: "unmerges merged block files into one-block-files",
 	Args:  cobra.ExactArgs(4),
 	RunE:  unmergeBlocksE,
@@ -51,15 +51,19 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("stop block must be greater than start block")
 	}
 
-	startFrom := fmt.Sprintf("%010d", start-(start%100))
-	err = srcStore.WalkFrom(ctx, "", startFrom, func(filename string) error {
-		zlog.Debug("checking 100-block file", zap.String("filename", filename))
+	startWalkFrom := fmt.Sprintf("%010d", start-(start%100))
+	err = srcStore.WalkFrom(ctx, "", startWalkFrom, func(filename string) error {
+		zlog.Debug("checking merged block file", zap.String("filename", filename))
+
 		startBlock := mustParseUint64(filename)
+
 		if startBlock > stop {
+			zlog.Debug("skipping merged block file", zap.String("reason", "past stop block"), zap.String("filename", filename))
 			return io.EOF
 		}
 
 		if startBlock+100 < start {
+			zlog.Debug("skipping merged block file", zap.String("reason", "before start block"), zap.String("filename", filename))
 			return nil
 		}
 
@@ -69,14 +73,14 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 		}
 		defer rc.Close()
 
-		reader, err := bstream.GetBlockReaderFactory.New(rc)
+		br, err := bstream.GetBlockReaderFactory.New(rc)
 		if err != nil {
-			return fmt.Errorf("get block reader: %w", err)
+			return fmt.Errorf("creating block reader: %w", err)
 		}
 
 		// iterate through the blocks in the file
 		for {
-			block, err := reader.Read()
+			block, err := br.Read()
 			if err == io.EOF {
 				break
 			}
@@ -94,7 +98,7 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 
 			pr, pw := io.Pipe()
 
-			//write block data to pipe
+			//write block data to pipe, and then close to signal end of data
 			go func(block *bstream.Block) {
 				var err error
 				defer func() {
@@ -104,13 +108,13 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 				var bw bstream.BlockWriter
 				bw, err = bstream.GetBlockWriterFactory.New(pw)
 				if err != nil {
-					zlog.Error("error creating block writer", zap.Error(err))
+					zlog.Error("creating block writer", zap.Error(err))
 					return
 				}
 
 				err = bw.Write(block)
 				if err != nil {
-					zlog.Error("error writing block", zap.Error(err))
+					zlog.Error("writing block", zap.Error(err))
 					return
 				}
 			}(block)
@@ -118,7 +122,7 @@ func unmergeBlocksE(cmd *cobra.Command, args []string) error {
 			//read block data from pipe and write block data to dest store
 			err = destStore.WriteObject(ctx, oneblockFilename, pr)
 			if err != nil {
-				return fmt.Errorf("error writing block %d to %s: %w", block.Number, oneblockFilename, err)
+				return fmt.Errorf("writing block %d to %s: %w", block.Number, oneblockFilename, err)
 			}
 
 			zlog.Info("wrote block", zap.Uint64("block_num", block.Number), zap.String("filename", oneblockFilename))
