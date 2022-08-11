38585d7d8f40c297969eb2ff41bc043fc7ca1cd0
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-08-11 12:22:30 -0400

    pbeth block normalization removed from decoder

--- types/decoder.go
+++ types/decoder.go
@@ -43,41 +43,5 @@ func BlockDecoder(blk *bstream.Block) (interface{}, error) {
 		return nil, fmt.Errorf("unable to decode payload: %s", err)
 	}
 
-	NormalizeBlockInPlace(block)
 	return block, nil
-}
-
-func NormalizeBlockInPlace(block *pbeth.Block) {
-	// This whole BlockDecoder method is being called through the `bstream.Block.ToNative()`
-	// method. Hence, it's a great place to add temporary data normalization calls to backport
-	// some features that were not in all blocks yet (because we did not re-process all blocks
-	// yet).
-	//
-	// Thoughts for the future: Ideally, we would leverage the version information here to take
-	// a decision, like `do X if version <= 2.1` so we would not pay the performance hit
-	// automatically instead of having to re-deploy a new version of bstream (which means
-	// rebuild everything mostly)
-	//
-	// We reconstruct the state reverted value per call, for each transaction traces. We also
-	// normalize signature curve points since we were not setting to be alwasy 32 bytes long and
-	// sometimes, it would have been only 31 bytes long.
-	for _, trx := range block.TransactionTraces {
-		trx.PopulateStateReverted()
-		trx.PopulateTrxStatus()
-
-		if len(trx.R) > 0 && len(trx.R) != 32 {
-			trx.R = NormalizeSignaturePoint(trx.R)
-		}
-
-		if len(trx.S) > 0 && len(trx.S) != 32 {
-			trx.S = NormalizeSignaturePoint(trx.S)
-		}
-	}
-
-	// We leverage StateReverted field inside the `PopulateLogBlockIndices`
-	// and as such, it must be invoked after the `PopulateStateReverted` has
-	// been executed.
-	if err := block.PopulateLogBlockIndices(); err != nil {
-		panic(fmt.Errorf("normalizing log block indices: %w", err))
-	}
 }
--- types/pb/sf/ethereum/type/v1/type.go
+++ types/pb/sf/ethereum/type/v1/type.go
@@ -195,6 +195,49 @@ func isPolygonException(log *Log) bool {
 	return bytes.Equal(log.Address, polygonFeeSystemAddress) && len(log.Topics) == 4 && bytes.Equal(log.Topics[0], polygonNeverRevertedTopic)
 }
 
+// NormalizeBlockInPlace
+func (block *Block) NormalizeInPlace() {
+	// We reconstruct the state reverted value per call, for each transaction traces. We also
+	// normalize signature curve points since we were not setting to be alwasy 32 bytes long and
+	// sometimes, it would have been only 31 bytes long.
+	for _, trx := range block.TransactionTraces {
+		trx.PopulateStateReverted()
+		trx.PopulateTrxStatus()
+
+		if len(trx.R) > 0 && len(trx.R) != 32 {
+			trx.R = NormalizeSignaturePoint(trx.R)
+		}
+
+		if len(trx.S) > 0 && len(trx.S) != 32 {
+			trx.S = NormalizeSignaturePoint(trx.S)
+		}
+	}
+
+	// We leverage StateReverted field inside the `PopulateLogBlockIndices`
+	// and as such, it must be invoked after the `PopulateStateReverted` has
+	// been executed.
+	if err := block.PopulateLogBlockIndices(); err != nil {
+		panic(fmt.Errorf("normalizing log block indices: %w", err))
+	}
+}
+
+func NormalizeSignaturePoint(value []byte) []byte {
+	if len(value) == 0 {
+		return value
+	}
+
+	if len(value) < 32 {
+		offset := 32 - len(value)
+
+		out := make([]byte, 32)
+		copy(out[offset:32], value)
+
+		return out
+	}
+
+	return value[0:32]
+}
+
 // PopulateLogBlockIndices fixes the `TransactionReceipt.Logs[].BlockIndex`
 // that is not properly populated by our deep mind instrumentation.
 func (block *Block) PopulateLogBlockIndices() error {
