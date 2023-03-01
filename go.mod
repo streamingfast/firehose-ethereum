d5f3c2b850dc6cf221c3bc25487dac5c99470c52
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2023-03-01 07:57:05 -0500

    tweak upgrade-merged-blocks to re-apply v2->v3 fix
    since blocks that were upgraded from v2 to v3 using the
    upgrade-merged-blocks tool will have incorrect "caller"
    on a nested delegate_call, the upgrade tool will now re-apply upgrade
    from version 2 to 3 on every block version 3.
    
    Note: Blocks version 3 produced directly from an instrumented geth node will NOT present the issue.
    
    * nested delegate_call issue:
    
    In the following scenario: `1. acct.abc --> 2. DELEGATE(acct.def) --> 3. DELEGATE(acct.ghi)`
    The *caller* of call 3 should be account `abc`, not account `def`.

--- go.mod
+++ go.mod
@@ -25,7 +25,7 @@ require (
 	github.com/streamingfast/dstore v0.1.1-0.20230202164314-93694544e2ca
 	github.com/streamingfast/eth-go v0.0.0-20230207194511-7d7968462321
 	github.com/streamingfast/firehose v0.1.1-0.20230221145342-8772b901f591
-	github.com/streamingfast/firehose-ethereum/types v0.0.0-20221129151535-42f0a2b8d76d
+	github.com/streamingfast/firehose-ethereum/types v0.0.0-20230301124208-8d73ea37b7ca
 	github.com/streamingfast/index-builder v0.0.0-20221031203737-fa2e70f09dc2
 	github.com/streamingfast/jsonpb v0.0.0-20210811021341-3670f0aa02d0
 	github.com/streamingfast/logging v0.0.0-20221209193439-bff11742bf4c
--- tools/upgrade-merged-blocks.go
+++ tools/upgrade-merged-blocks.go
@@ -22,10 +22,18 @@ func normalize(in *bstream.Block) (*bstream.Block, error) {
 	block := in.ToProtocol().(*pbeth.Block)
 
 	prevVersion := block.Ver
+	// this is a hack for a previous incomplete normalize call regarding nested DELEGATE calls. the v2->v3 upgrade is idempotent
+	if block.Ver == 3 {
+		block.Ver = 2
+	}
 	block.NormalizeInPlace()
 	logUpgrade.Do(func() {
 		if prevVersion == block.Ver {
-			zlog.Error("nothing to do: previous version is the same as available version", zap.Int32("version", prevVersion))
+			if prevVersion == 3 {
+				zlog.Info("Re-applying normalize version 2->3")
+			} else {
+				zlog.Error("nothing to do on first block: previous version is the same as available version", zap.Int32("version", prevVersion), zap.Uint64("first_block", block.Number))
+			}
 		}
 		zlog.Info("Upgrading ethereum blocks",
 			zap.Int32("previous_version", prevVersion),
