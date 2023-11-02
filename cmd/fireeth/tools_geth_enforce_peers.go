31a3bd36635063620a3ceb05138fd7e9fbc8df8b
Matthieu Vachon <matt@streamingfast.io>
2023-11-02 15:00:38 -0400

    Added example of how to call enforce peers and enabled `--once` mode for single time call

--- cmd/fireeth/tools_geth_enforce_peers.go
+++ cmd/fireeth/tools_geth_enforce_peers.go
@@ -16,6 +16,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
 	"net"
@@ -26,6 +27,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/bobg/go-generics/v2/slices"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/streamingfast/bstream"
@@ -46,12 +48,16 @@ func registerGethEnforcePeersCmd(parent *cobra.Command, binary string, logger *z
 				"enforce-peers",
 				"Enforce provided peers to be connected to the node, this tools is meant to run as a sidecar to a Geth node",
 				Flags(func(flags *pflag.FlagSet) {
+					flags.BoolP("once", "o", false, "Run as a unit operation and avoid launching a long running process")
 					flags.String("ipc-file-path", "", "Path to the IPC file to connect to")
 					flags.StringArray("ensure-peers", nil, "List of peers to ensure connection to")
-
 				}),
 				ExamplePrefixed(fmt.Sprintf("%s tools geth enforce-peers", binary), `
+					# Run as a long running process
+					--ipc-file-path=/var/data/geth/ipc --ensure-peers=<hostname>[:<port>]
 
+					# Run only once instead of launching a long running process
+					--ipc-file-path=/var/data/geth/ipc --ensure-peers=<hostname>[:<port>] --once
 				`),
 			),
 		),
@@ -78,24 +84,66 @@ func createGethEnforcePeersE(logger *zap.Logger, tracer logging.Tracer) firecore
 			Shutter:              shutter.New(),
 			node:                 node,
 			wantedPeersHostnames: sflags.MustGetStringArray(cmd, "ensure-peers"),
+			wantedPeers:          map[string]*Enode{},
 			logger:               logger.Named("enforcer"),
 		}
 
 		cli.Ensure(len(peersEnforcer.wantedPeersHostnames) > 0, "--ensure-peers is required")
 
-		app := cli.NewApplication(cmd.Context())
+		if sflags.MustGetBool(cmd, "once") {
+			return runOnce(node, monitor, peersEnforcer)
+		}
 
-		logger.Info("starting Geth monitor")
-		app.SuperviseAndStart(monitor)
-
-		logger.Info("starting Geth peers enforcer")
-		app.SuperviseAndStart(peersEnforcer)
-
-		logger.Info("waiting for termination signal")
-		app.WaitForTermination(logger, 0, 0)
-
-		return nil
+		return runDaemon(cmd.Context(), logger, monitor, peersEnforcer)
 	}
+}
+
+func runOnce(node *GethNode, monitor *GethMonitor, enforcer *GethPeersEnforcer) error {
+	if err := monitor.runOnce(); err != nil {
+		return fmt.Errorf("cannot run monitor once: %w", err)
+	}
+
+	if err := enforcer.runOnce(); err != nil {
+		return fmt.Errorf("cannot run peers enforcer once: %w", err)
+	}
+
+	// The second run of the monitor is to ensure that connected peers were recorded
+	if err := monitor.runOnce(); err != nil {
+		return fmt.Errorf("cannot run monitor once: %w", err)
+	}
+
+	fmt.Printf("Connected peers (%d)\n", len(node.connectedPeers))
+	for _, peer := range node.connectedPeers {
+		peerEnode, err := parseEnode(peer)
+		if err != nil {
+			fmt.Println("- " + peer)
+			continue
+		}
+
+		suffix := ""
+		if _, found := enforcer.wantedPeers[peerEnode.ID]; found {
+			suffix = " (wanted)"
+		}
+
+		fmt.Println("- " + peer + suffix)
+	}
+
+	return nil
+}
+
+func runDaemon(ctx context.Context, logger *zap.Logger, monitor *GethMonitor, enforcer *GethPeersEnforcer) error {
+	app := cli.NewApplication(ctx)
+
+	logger.Info("starting Geth monitor")
+	app.SuperviseAndStart(monitor)
+
+	logger.Info("starting Geth peers enforcer")
+	app.SuperviseAndStart(enforcer)
+
+	logger.Info("waiting for termination signal")
+	app.WaitForTermination(logger, 0, 0)
+
+	return nil
 }
 
 var enodeRegexp = regexp.MustCompile(`enode://([a-f0-9]*)@.*$`)
@@ -148,6 +196,7 @@ type GethPeersEnforcer struct {
 
 	node                 *GethNode
 	wantedPeersHostnames []string
+	wantedPeers          map[string]*Enode
 	logger               *zap.Logger
 }
 
@@ -158,37 +207,46 @@ func (s *GethPeersEnforcer) Run() {
 	for {
 		select {
 		case <-s.Terminating():
-			s.logger.Info("geth PeersEnforcer terminated")
+			s.logger.Info("geth peers enforced terminated")
 			return
 		case <-time.After(10 * time.Second):
 		}
 
-		if len(s.node.enodeStr) < 20 {
-			s.logger.Info("wrong enode string will retry", zap.String("enode", s.node.enodeStr))
-			continue
-		}
-
-		allEnodes := map[string]bool{}
-		for _, hostname := range s.wantedPeersHostnames {
-			enodes := s.getEnodesFromPeers(hostname)
-			s.logger.Debug("got enode", zap.String("hostname", hostname), zap.Strings("enodes", enodes))
-
-			for _, enode := range enodes {
-				allEnodes[enode] = true
-			}
-		}
-
-		for enode := range allEnodes {
-			if err := s.addPeer(enode); err != nil {
-				s.logger.Warn("cannot add peer", zap.String("enode", enode))
-			}
+		if err := s.runOnce(); err != nil {
+			s.logger.Warn("enforce peer run failed", zap.Error(err))
 		}
 	}
 }
 
+func (s *GethPeersEnforcer) runOnce() error {
+	if len(s.node.enodeStr) < 20 {
+		s.logger.Info("wrong enode string will retry", zap.String("enode", s.node.enodeStr))
+		return nil
+	}
+
+	for _, hostname := range s.wantedPeersHostnames {
+		enodes := s.getEnodesFromPeers(hostname)
+
+		enodesString := slices.Map(enodes, func(e *Enode) string { return e.String() })
+		s.logger.Debug("got enode", zap.String("hostname", hostname), zap.Strings("enodes", enodesString))
+
+		for _, enode := range enodes {
+			s.wantedPeers[enode.ID] = enode
+		}
+	}
+
+	for _, enode := range s.wantedPeers {
+		if err := s.addPeer(enode); err != nil {
+			return fmt.Errorf("cannot add peer: %w", err)
+		}
+	}
+
+	return nil
+}
+
 // AddPeer sends a command through IPC socket to connect geth to the given peer
-func (s *GethPeersEnforcer) addPeer(peer string) error {
-	if s.mustIgnorePeer(peer) {
+func (s *GethPeersEnforcer) addPeer(peer *Enode) error {
+	if s.mustIgnorePeer(peer.Full) {
 		return nil
 	}
 
@@ -223,7 +281,36 @@ func (s *GethPeersEnforcer) mustIgnorePeer(peer string) bool {
 	return false
 }
 
-func (s *GethPeersEnforcer) getEnodesFromPeers(hostname string) []string {
+type Enode struct {
+	Full string
+	ID   string
+	IP   string
+}
+
+func (e *Enode) String() string {
+	return e.Full
+}
+
+func parseEnode(in string) (*Enode, error) {
+	// enode://e4d5433fd9e84930cd38028f2fdb1ca8d55bdb7b6a749da57e8aa7fc5d3c146c44c0d129a14cecc7b0f13bb98700bf392dd4fd7c31bf2fe26038d4ba8f5a8e32@127.0.0.1:30303
+	if !strings.HasPrefix(in, "enode://") {
+		return nil, fmt.Errorf("invalid enode string, must start with 'enode://': %s", in)
+	}
+
+	raw := strings.TrimPrefix(in, "enode://")
+	id, ip, ok := strings.Cut(raw, "@")
+	if !ok {
+		return nil, fmt.Errorf("invalid enode string, must have '@' separator: %s", in)
+	}
+
+	return &Enode{
+		Full: in,
+		ID:   id,
+		IP:   ip,
+	}, nil
+}
+
+func (s *GethPeersEnforcer) getEnodesFromPeers(hostname string) []*Enode {
 	port := "8545"
 	if splitted := strings.Split(hostname, ":"); len(splitted) == 2 {
 		port = splitted[1]
@@ -237,7 +324,8 @@ func (s *GethPeersEnforcer) getEnodesFromPeers(hostname string) []string {
 		s.logger.Warn("cannot get IP for hostname", zap.Error(err), zap.String("hostname", hostname))
 		return nil
 	}
-	var enodes []string
+
+	var enodes []*Enode
 	for _, ip := range ips {
 		endpoint := fmt.Sprintf("http://%s:%s", ip, port)
 		body := `{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":1}`
@@ -249,11 +337,14 @@ func (s *GethPeersEnforcer) getEnodesFromPeers(hostname string) []string {
 		}
 
 		enodeStr := gjson.Get(enodeAddr, "result.enode").String()
-		if !strings.HasPrefix(enodeStr, "enode://") {
-			s.logger.Warn("got invalid enode string from IP", zap.Stringer("ip", ip), zap.String("enode", enodeStr))
+
+		enode, err := parseEnode(enodeStr)
+		if err != nil {
+			s.logger.Warn("got invalid enode string from IP", zap.String("enode", enodeStr), zap.Error(err))
 			continue
 		}
-		enodes = append(enodes, enodeStr)
+
+		enodes = append(enodes, enode)
 	}
 	return enodes
 }
@@ -268,84 +359,90 @@ type GethMonitor struct {
 
 // Monitor periodically checks the head block num and block time, as well as the enode string (server ID)
 func (s *GethMonitor) Run() {
-	started := time.Now()
-
 	var lastLog *time.Time
 
 	for {
 		select {
 		case <-s.Terminating():
-			s.logger.Info("geth Monitor terminated")
+			s.logger.Info("geth monitor terminated")
 			return
 		case <-time.After(2 * time.Second):
 		}
 
-		resp, err := s.node.sendGethCommand(`{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":1}`)
-		if err != nil {
-			s.logger.Warn("geth Monitor cannot get info from IPC socket", zap.Error(err))
-			if time.Since(started) < time.Minute {
-				continue
-			}
-		} else {
-			enodeStr := gjson.Get(resp, "result.enode").String()
-
-			fields := []zap.Field{zap.String("enode", enodeStr)}
-			if s.tracer.Enabled() {
-				fields = append(fields, zap.Reflect("resp", resp))
-			}
-
-			s.logger.Debug("geth Monitor got node info", fields...)
-			s.node.setEnodeStr(enodeStr)
-		}
-
-		resp, err = s.node.sendGethCommand(`{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":1}`)
-		if err != nil {
-			s.logger.Warn("geth Monitor cannot get peers from IPC socket", zap.Error(err))
-		} else {
-			connectedPeers := []string{}
-			for _, peer := range gjson.Get(resp, "result").Array() {
-				connectedPeers = append(connectedPeers, peer.Get("enode").String())
-			}
-			s.node.peerMutex.Lock()
-			s.node.connectedPeers = connectedPeers
-			s.node.peerMutex.Unlock()
-		}
-
-		resp, err = s.node.sendGethCommand(`{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}`)
-		if err != nil {
-			s.logger.Warn("geth Monitor cannot get blocknumber from IPC socket", zap.Error(err))
-			continue
-		}
-		lastBlock := gjson.Get(resp, "result")
-		lastBlockNum := hex2uint(lastBlock.String())
-		if lastBlockNum == 0 {
-			continue
-		}
-
-		resp, err = s.node.sendGethCommand(fmt.Sprintf(`{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["%s", true],"id":1}`, lastBlock))
-		if err != nil {
-			s.logger.Warn("geth Monitor cannot get block by number", zap.Error(err))
-			continue
-		}
-		timestamp := time.Unix(hex2int(gjson.Get(resp, "result.timestamp").String()), 0)
-		hash := hex2string(gjson.Get(resp, "result.hash").String())
-
-		s.node.lastBlockMutex.Lock()
-		s.node.lastBlock = &bstream.Block{
-			Id:        hash,
-			Number:    uint64(lastBlockNum),
-			Timestamp: timestamp,
+		if err := s.runOnce(); err != nil {
+			s.logger.Warn("geth monitor run failed", zap.Error(err))
 		}
 
 		if lastLog == nil || time.Since(*lastLog) > time.Minute {
-			s.logger.Info("monitor last block seen", zap.Stringer("block", s.node.lastBlock.AsRef()))
+			if s.node.lastBlock == nil {
+				s.logger.Info("geth monitor never seen last block yet")
+			} else {
+				s.logger.Info("geth monitor last block seen", zap.Stringer("block", s.node.lastBlock.AsRef()))
+			}
 
 			now := time.Now()
 			lastLog = &now
 		}
-
-		s.node.lastBlockMutex.Unlock()
 	}
+}
+
+func (s *GethMonitor) runOnce() error {
+	resp, err := s.node.sendGethCommand(`{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":1}`)
+	if err != nil {
+		return fmt.Errorf("cannot get info from IPC socket: %w", err)
+	}
+
+	enodeStr := gjson.Get(resp, "result.enode").String()
+
+	fields := []zap.Field{zap.String("enode", enodeStr)}
+	if s.tracer.Enabled() {
+		fields = append(fields, zap.Reflect("resp", resp))
+	}
+
+	s.logger.Debug("got node info", fields...)
+	s.node.setEnodeStr(enodeStr)
+
+	resp, err = s.node.sendGethCommand(`{"jsonrpc":"2.0","method":"admin_peers","params":[],"id":1}`)
+	if err != nil {
+		return fmt.Errorf("cannot get peers from IPC socket: %w", err)
+	}
+
+	connectedPeers := []string{}
+	for _, peer := range gjson.Get(resp, "result").Array() {
+		connectedPeers = append(connectedPeers, peer.Get("enode").String())
+	}
+	s.node.peerMutex.Lock()
+	s.node.connectedPeers = connectedPeers
+	s.node.peerMutex.Unlock()
+
+	resp, err = s.node.sendGethCommand(`{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}`)
+	if err != nil {
+		return fmt.Errorf("cannot get blocknumber from IPC socket: %w", err)
+	}
+
+	lastBlock := gjson.Get(resp, "result")
+	lastBlockNum := hex2uint(lastBlock.String())
+	if lastBlockNum == 0 {
+		return fmt.Errorf("last block is 0, skipping")
+	}
+
+	resp, err = s.node.sendGethCommand(fmt.Sprintf(`{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["%s", true],"id":1}`, lastBlock))
+	if err != nil {
+		return fmt.Errorf("cannot get block by number: %w", err)
+	}
+
+	timestamp := time.Unix(hex2int(gjson.Get(resp, "result.timestamp").String()), 0)
+	hash := hex2string(gjson.Get(resp, "result.hash").String())
+
+	s.node.lastBlockMutex.Lock()
+	s.node.lastBlock = &bstream.Block{
+		Id:        hash,
+		Number:    uint64(lastBlockNum),
+		Timestamp: timestamp,
+	}
+	s.node.lastBlockMutex.Unlock()
+
+	return nil
 }
 
 // cannot use ReadAll on an IPC socket
