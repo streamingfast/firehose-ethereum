9f2a51b6c2e4ba47eaf45f38d2cddd2c70825c93
billettc <charles@cbillette.com>
2023-11-23 15:46:15 -0500

    Add Optimism poller command and refactor polling functions
    The commit refactors functions in 'fireeth' command-line interface (CLI) to adapt to changes in the 'firehose-core' (firehose dependency). This includes addition of an Optimism block poller command that triggers polling from the Optimism RPC , thus leading to the fetching and handling of Ethereum blocks. Smaller changes include modifications in 'go.mod' and 'go.sum' to reflect updated and/or replaced dependencies.

--- cmd/fireeth/main.go
+++ cmd/fireeth/main.go
@@ -104,6 +104,7 @@ func Chain() *firecore.Chain[*pbeth.Block] {
 				parent.AddCommand(newFixPolygonIndexCmd(zlog))
 				parent.AddCommand(newPollRPCBlocksCmd(zlog))
 				parent.AddCommand(newPollerCmd(zlog, tracer))
+				parent.AddCommand(newOptimismPollerCmd(zlog, tracer))
 
 				registerGethEnforcePeersCmd(parent, chain.BinaryName(), zlog, tracer)
 
--- cmd/fireeth/poller.go
+++ cmd/fireeth/poller.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
-	firecore "github.com/streamingfast/firehose-core"
-	"github.com/streamingfast/logging"
+	"fmt"
+	"path"
+	"strconv"
+	"time"
 
+	"github.com/spf13/cobra"
+	"github.com/streamingfast/bstream"
+	"github.com/streamingfast/eth-go/rpc"
+	firecore "github.com/streamingfast/firehose-core"
+	"github.com/streamingfast/firehose-core/blockpoller"
+	"github.com/streamingfast/firehose-ethereum/blockfetcher"
+	"github.com/streamingfast/logging"
 	"go.uber.org/zap"
 )
 
@@ -12,17 +20,54 @@ func newPollerCmd(logger *zap.Logger, tracer logging.Tracer) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "poller",
 		Short: "poll blocks from different sources",
+	}
+
+	cmd.AddCommand(newOptimismPollerCmd(logger, tracer))
+	return cmd
+}
+
+func newOptimismPollerCmd(logger *zap.Logger, tracer logging.Tracer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "optimism <rpc-endpoint> <first-streamable-block>",
+		Short: "poll blocks from optimism rpc",
 		Args:  cobra.ExactArgs(2),
-		RunE:  pollerRunE(logger, tracer),
+		RunE:  optimismPollerRunE(logger, tracer),
 	}
 
 	return cmd
 }
 
-func pollerRunE(logger *zap.Logger, tracer logging.Tracer) firecore.CommandExecutor {
+func optimismPollerRunE(logger *zap.Logger, tracer logging.Tracer) firecore.CommandExecutor {
 	return func(cmd *cobra.Command, args []string) (err error) {
-		//ctx := cmd.Context()
-		//blockpoller := blockpoller.New(
+		ctx := cmd.Context()
+
+		rpcEndpoint := args[0]
+		dataDir := cmd.Flag("data-dir").Value.String()
+		stateDir := path.Join(dataDir, "poller-state")
+
+		logger.Info("launching firehose-ethereum poller", zap.String("rpc_endpoint", rpcEndpoint), zap.String("state_dir", stateDir))
+
+		rpcClient := rpc.NewClient(rpcEndpoint)
+
+		firstStreamableBlock, err := strconv.ParseUint(args[1], 10, 64)
+		if err != nil {
+			return fmt.Errorf("unable to parse first streamable block %d: %w", firstStreamableBlock, err)
+		}
+
+		fetcher := blockfetcher.NewOptimismBlockFetcher(rpcClient, 1*time.Second)
+		handler := blockpoller.NewFireBlockHandler("type.googleapis.com/sf.ethereum.type.v2.Block")
+		poller := blockpoller.New(fetcher, handler, blockpoller.WithStoringState(stateDir), blockpoller.WithLogger(logger))
+
+		latestBlock, err := rpcClient.GetBlockByNumber(ctx, rpc.FinalizedBlock)
+		if err != nil {
+			return fmt.Errorf("getting latest block: %w", err)
+		}
+
+		err = poller.Run(ctx, firstStreamableBlock, bstream.NewBlockRef(latestBlock.Hash.String(), uint64(latestBlock.Number)))
+		if err != nil {
+			return fmt.Errorf("running poller: %w", err)
+		}
+
 		return nil
 	}
 }
