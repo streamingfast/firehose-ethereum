c096fe778a36d1eb486358e8600d416be32bcd13
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2026-04-28 11:57:15 -0400

    add  FIREETH_TOOLS_COMPARE_IGNORE_OPSTACK_SYSTEM_CALLS_ORDER to comparison

--- cmd/fireeth/tools_sanitizer.go
+++ cmd/fireeth/tools_sanitizer.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"sort"
 
 	pbbstream "github.com/streamingfast/bstream/pb/sf/bstream/v1"
 	firecore "github.com/streamingfast/firehose-core"
@@ -10,6 +12,7 @@ import (
 )
 
 var ignoreOrdinals = os.Getenv("FIREETH_TOOLS_COMPARE_IGNORE_ORDINALS") == "true"
+var ignoreOPStackSystemCallsOrder = os.Getenv("FIREETH_TOOLS_COMPARE_IGNORE_OPSTACK_SYSTEM_CALLS_ORDER") == "true"
 
 func SanitizeEthereumBlockForCompare(block *pbbstream.Block) *pbbstream.Block {
 	untypedEthBlock, err := block.Payload.UnmarshalNew()
@@ -44,6 +47,30 @@ func SanitizeEthereumBlockForCompare(block *pbbstream.Block) *pbbstream.Block {
 		}
 		for _, c := range ethBlock.CodeChanges {
 			c.Ordinal = 0
+		}
+	}
+	if ignoreOPStackSystemCallsOrder {
+		// reorder system calls by using all the fields as sorting keys
+		sort.Slice(ethBlock.SystemCalls, func(i, j int) bool {
+			si, sj := ethBlock.SystemCalls[i], ethBlock.SystemCalls[j]
+			if cmp := bytes.Compare(si.Address, sj.Address); cmp != 0 {
+				return cmp < 0
+			}
+			if cmp := bytes.Compare(si.Caller, sj.Caller); cmp != 0 {
+				return cmp < 0
+			}
+			if cmp := bytes.Compare(si.AddressDelegatesTo, sj.AddressDelegatesTo); cmp != 0 {
+				return cmp < 0
+			}
+			if si.GasConsumed != sj.GasConsumed {
+				return si.GasConsumed < sj.GasConsumed
+			}
+			return si.GasLimit < sj.GasLimit
+		})
+		for _, call := range ethBlock.SystemCalls {
+			call.BeginOrdinal = 0
+			call.EndOrdinal = 0
+			call.Index = 1
 		}
 	}
 	for _, tx := range ethBlock.TransactionTraces {
